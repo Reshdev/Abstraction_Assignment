c16e7cb3995670d6aec819c5d3e1b5ff7de88200
Reshdev <135739646+Reshdev@users.noreply.github.com>
2023-06-24 08:23:25 +0530

    Added static file

---
+++ AddStatic.java
@@ -0,0 +1,15 @@
+class AddStatic
+{
+	public static int add()
+	{
+		int a=12;
+		int b=10;
+		int c=a+b;
+		return c;
+	}
+	public static void main(String args[])
+	{
+		
+		System.out.print(AddStatic.add());
+	}
+}
